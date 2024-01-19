c28a63cecb7379736cde5a613ebad2f59881112a
Amr Adel <amr96.adel@gmail.com>
2024-01-19 20:19:07 +0200

    TASK 1 -1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,16 +1,17 @@
+#1/usr/bin/pup
 # install flask from pip3.
 package { 'python3-pip':
   ensure => installed,
 }
 
-exec { 'install_flask':
+package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip',
   require  => Package['python3-pip'],
 }
 
 package { 'werkzeug':
-  ensure   => '2.0.1',
+  ensure   => '2.1.1',
   provider => 'pip',
   require  => Package['python3-pip'],
 }
